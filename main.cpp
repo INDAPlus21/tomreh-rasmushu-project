fcf507c81ed4beddc8fa5923959f4cd777e2fdca
Tom Rehnström <tomreh@ug.kth.se>
2022-03-28 19:51:15 +0200

    Added cross-platform compatability to premake.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
 
 int main() {
     using namespace std;
 
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    };
+
     cout << "Hello world" << endl;
+
+    glfwTerminate();
     return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,56 @@
 workspace "tomreh-rasmushu-project"
+   location "project"
    configurations { "Debug", "Release" }
+   platforms { "Win32", "x64", "macosx" }
 
-project "tomreh-rasmushu-project"
+   filter { "platforms:Win32" }
+      system "Windows"
+      architecture "x86"
+
+   filter { "platforms:x64" }
+      system "Windows"
+      architecture "x64"
+
+   filter { "platforms:macosx" }
+      system "macosx"
+      architecture "x64"
+
+   filter { }
+
+
+project "Project Rayman"
    kind "ConsoleApp"
    language "C++"
-   targetdir "bin/%{cfg.buildcfg}"
+   location "project/Project Rayman"
+   targetdir "bin/%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}"
 
    files { "**.h", "**.cpp" }
+
+   includedirs {
+      "dep/include",
+   }
+
+      links {
+         "glfw"
+      }
+
+      filter "system:Windows"
+         filter "architecture:x86"
+            libdirs {
+               "dep/win32"
+            }
+      
+      filter "system:Windows"
+         filter "architecture:x64"
+            libdirs {
+               "dep/x64"
+            }
+
+      filter "system:macosx"
+         filter "architecture:x64"
+            libdirs {
+               "dep/macos"
+            }
 
    filter "configurations:Debug"
       defines { "DEBUG" }
@@ -15,3 +59,5 @@ project "tomreh-rasmushu-project"
    filter "configurations:Release"
       defines { "NDEBUG" }
       optimize "On"
+   
+   filter { }
