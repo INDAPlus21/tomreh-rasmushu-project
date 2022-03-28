9ab4f294868c258fa518f1d8a0edba9d956f6e14
Tom Rehnström <tomreh@ug.kth.se>
2022-03-28 16:00:56 +0200

    Add basic premake config with hello world

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "tomreh-rasmushu-project"
+   configurations { "Debug", "Release" }
+
+project "tomreh-rasmushu-project"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
