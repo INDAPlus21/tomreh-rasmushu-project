9663fd229c6eab2dc7cfa9fdced4e588885076e8
Tom <tomilomen@gmail.com>
2022-03-29 19:30:28 +0200

    Fixed so works on windows(MinGW) and macos

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,8 @@
 workspace "tomreh-rasmushu-project"
    configurations { "Debug", "Release" }
-   platforms { "MacOS", "x64", "Win32" }
+   platforms { "Win32", "MacOS", "x64"}
 
-   filter { "platforms:Win32" }
+   filter "platforms:Win32"
       system "windows"
       architecture "x86"
 
@@ -25,9 +25,19 @@ project "Rayman"
    files { "**.h", "**.cpp" }
 
    includedirs {
-      "./dep/glfw-3.3.6/include",
-      "./dep/glew-2.2.0_1/include",
+      "dep/glfw-3.3.6/include",
+      "dep/glew-2.2.0_1/include",
    }
+
+      filter { "system:windows" }
+         links {
+            "glfw3",
+            "glew32s",
+            "user32",
+            "gdi32",
+            "kernel32",
+            "opengl32"
+         }
 
       filter { "system:macosx" }
          links {
@@ -36,25 +46,18 @@ project "Rayman"
             "OpenGL.framework"
          }
 
-      filter { "system:windows" }
-         links {
-            "glew32s",
-            "opengl32",
-            "glfw3"
-         }
-
       filter { "system:windows" } 
          filter "architecture:x86"
             libdirs {
-               "./dep/glfw-3.3.6/win32",
-               "./dep/glew-2.2.0_1/win32"
+               "dep/glfw-3.3.6/win32",
+               "dep/glew-2.2.0_1/win32"
             }
       
       filter { "system:windows" }
          filter "architecture:x64"
             libdirs {
-               "dep/glfw-3.3.6/x64",
-               "dep/glew-2.2.0_1/x64"
+               "./dep/glfw-3.3.6/x64",
+               "./dep/glew-2.2.0_1/x64"
             }
 
       filter { "system:macosx" }
