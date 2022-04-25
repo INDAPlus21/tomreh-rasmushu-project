f033ed90135fc7321eaf7f4940230c16f7082740
Tom Rehnström <tomreh@ug.kth.se>
2022-04-25 23:04:45 +0200

    Half-baked ECS

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -6,13 +6,61 @@
 
 #include "shader.h"
 #include "game.h"
-
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
+#include "framebuffer.h"
+#include "primatives.h"
 
 const char *vertexShaderSource = "res/shaders/vertex.vert";
 const char *fragmentShaderSource = "res/shaders/fragment.frag";
 static GLFWwindow* s_window = nullptr;
+
+typedef struct VertexBufferElement
+{
+    uint type;
+    uint count;
+    unsigned char normalized;
+
+    static uint GetSizeOfType(uint type)
+    {
+        switch (type)
+        {
+            case GL_FLOAT: return 4;
+            case GL_UNSIGNED_INT: return 4;
+            case GL_UNSIGNED_BYTE: return 1;
+        }
+        return 0;
+    }
+};
+
+typedef struct Layout
+{
+    uint stride;
+    std::vector<VertexBufferElement> elements;
+};
+
+typedef struct DrawObject
+{
+    uint va;
+    uint vb;
+    uint ib;
+    bool pp;
+    uint *draw_program;
+    uint *fb;
+    Layout layout;
+};
+
+typedef struct FullscreenQuad
+{
+    uint va;
+    uint vb;
+    uint ib;
+};
+
+enum ObjectType
+{
+    triangle,
+    rectangle,
+    circle
+};
 
 static void glfwError(int id, const char* description)
 {
@@ -64,34 +112,63 @@ bool renderer_init()
     return true;
 }
 
-void CreateThings() 
+// x, y, width, height, ska vara i pixel koordinater
+DrawObject CreateDrawObject(ObjectType type, uint x, uint y, bool post_process,  uint width = 0, uint height = 0)
 {
-    unsigned int va;
-    unsigned int vb;
-    unsigned int ib;
-
-    float verts[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.5f,  0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
-    };
-
-    unsigned int indices[6] = {
-        0, 1, 2,
-        3, 2, 0
-    };
-
+    uint va, vb, ib, program;
+    float *verts;
+    uint *indices;
     Layout layout = {0, std::vector<VertexBufferElement>()};
+
+    if (type == rectangle)
+    {
+        verts = GenRectVertex(0, 0, 255, 255);
+        indices = GenRectIndex();
+    }
+
     GenVertexArray(&va);
     GenVertexBuffer(&vb, verts, 4 * 3 * sizeof(float));
     AddToLayout(layout, GL_FLOAT, 3);
     ConfigVertexArrayLayout(&va, &vb, layout);
-
     GenIndexBuffer(&ib, indices, 6);
+    program = CreateProgram(vertexShaderSource, fragmentShaderSource);
+    uint programs[2];
+    programs[0] = program;
+    uint fb_info[3] = {0, 0, 0};
 
-    GLuint program = CreateProgram(vertexShaderSource, fragmentShaderSource);
-    glUseProgram(program);
+    if (post_process)
+    {  
+        // Change shader sources to correct ones
+        uint pp_program = CreateProgram(vertexShaderSource, fragmentShaderSource);
+        programs[1] = pp_program;
+        GenRenderBuffer(&fb_info[0], WINDOW_WIDTH, WINDOW_HEIGHT);
+        GenTexture2D(&fb_info[1], WINDOW_WIDTH, WINDOW_HEIGHT);
+        GenFrameBuffer(&fb_info[2], fb_info[0], fb_info[1]);
+    }
+
+    DrawObject returnthing = {va, vb, ib, post_process, programs, fb_info, layout};
+}
+
+void DrawDrawObject(DrawObject &object)
+{
+
+}
+
+FullscreenQuad GenFullscreenRect()
+{
+    float verts[] = {
+        -1.0f, -1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, 1.0f, 1.0f,
+        -1.0f,  1.0f, 0.0f, 1.0f
+    };
+
+        uint indices[] = {
+        0, 1, 2,
+        3, 2, 0
+    };
+
+    
 }
 
 void renderer_prepare()
@@ -121,47 +198,47 @@ void renderer_clean_up()
     glfwTerminate();
 }
 
-void GenVertexBuffer(unsigned int *id, const void* data, unsigned int size) 
+void GenVertexBuffer(uint *id, const void* data, uint size) 
 {
     glGenBuffers(1, id);
     glBindBuffer(GL_ARRAY_BUFFER, *id);
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
 
-void DeleteVetexBuffer(unsigned int *id)
+void DeleteVetexBuffer(uint *id)
 {
     glDeleteBuffers(GL_ARRAY_BUFFER, id);
 }
 
-void GenVertexArray(unsigned int *id)
+void GenVertexArray(uint *id)
 {
     glGenVertexArrays(1, id);
 }
 
-void DeleteVertexArray(unsigned int *id)
+void DeleteVertexArray(uint *id)
 {
     glDeleteVertexArrays(1, id);
 }
 
-void GenIndexBuffer(unsigned int *id, const unsigned int *data, int count) 
+void GenIndexBuffer(uint *id, const uint *data, int count) 
 {
     glGenBuffers(1, id);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *id);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint), data, GL_STATIC_DRAW);
 }
 
-void DeleteIndexBuffer(unsigned int *id)
+void DeleteIndexBuffer(uint *id)
 {
     glDeleteBuffers(1, id);
 }
 
-void ConfigVertexArrayLayout(unsigned int *va, unsigned int *vb, const Layout &layout)
+void ConfigVertexArrayLayout(uint *va, uint *vb, const Layout &layout)
 {
     glBindBuffer(GL_ARRAY_BUFFER, *vb);
     glBindVertexArray(*va);
 
-    unsigned int offset = 0;
-    for (unsigned int i = 0; i < layout.elements.size(); i++)
+    uint offset = 0;
+    for (uint i = 0; i < layout.elements.size(); i++)
     {
         glEnableVertexAttribArray(i);
         glVertexAttribPointer(i, layout.elements[i].count,
@@ -174,7 +251,7 @@ void ConfigVertexArrayLayout(unsigned int *va, unsigned int *vb, const Layout &l
     
 }
 
-void AddToLayout(Layout &layout, GLuint type, unsigned int count, bool normalize)
+void AddToLayout(Layout &layout, GLuint type, uint count, bool normalize = false)
 {
     struct VertexBufferElement elem = {type, count, normalize};
 
--- src/renderer.h
+++ src/renderer.h
@@ -4,35 +4,17 @@
 #include <GL/glew.h>
 #include <vector>
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+typedef unsigned int uint;
+
 bool renderer_init();
 void renderer_prepare();
 void renderer_render();
 void renderer_present();
 void renderer_clean_up();
 
-struct VertexBufferElement
-{
-    unsigned int type;
-    unsigned int count;
-    unsigned char normalized;
-
-    static unsigned int GetSizeOfType(unsigned int type)
-    {
-        switch (type)
-        {
-            case GL_FLOAT: return 4;
-            case GL_UNSIGNED_INT: return 4;
-            case GL_UNSIGNED_BYTE: return 1;
-        }
-        return 0;
-    }
-};
-
-struct Layout
-{
-    unsigned int stride;
-    std::vector<VertexBufferElement> elements;
-};
 
 void CreateThings();
 void GenVertexBuffer(unsigned int *id, const void* data, unsigned int size);
@@ -42,4 +24,4 @@ void DeleteVertexArray(unsigned int *id);
 void GenIndexBuffer(unsigned int *id, const unsigned int *data, int count);
 void DeleteIndexBuffer(unsigned int *id);
 void ConfigVertexArrayLayout(unsigned int *va, unsigned int *vb, const Layout &layout);
-void AddToLayout(Layout &layout, GLuint type, unsigned int count, bool normalize = false);
+void AddToLayout(Layout &layout, GLuint type, unsigned int count, bool normalize);
